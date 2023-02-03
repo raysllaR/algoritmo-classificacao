45b4a11fed9b439de3554d344b71228424a15e09
Rayslla <rayslla.rodrigues.ferreira@gmail.com>
2023-02-02 22:09:01 -0300

    feat: importações das bibliotecas a serem utilizadas

---
+++ main.py
@@ -0,0 +1,6 @@
+from keras.models import Sequential  # modelo sequencial
+# Camanda de convolução, Pooling, Flatten, rede neoral densa
+from keras.layers import Conv2D, MaxPool2D, Flatten, Dense
+# Normalização do mapa de caracteristicas
+from keras.layers import BatchNormalization  # normalização das imagens
+from keras.preprocessing.image import ImageDataGenerator  # questionavel a utilização
