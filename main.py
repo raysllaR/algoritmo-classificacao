cdda010d85f5914cd0a87d3928cd63ba9934120a
Rayslla <rayslla.rodrigues.ferreira@gmail.com>
2023-02-02 22:17:43 -0300

    feat: add primeira camada  de convolução

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ from keras.layers import Conv2D, MaxPool2D, Flatten, Dense
 # Normalização do mapa de caracteristicas
 from keras.layers import BatchNormalization  # normalização das imagens
 from keras.preprocessing.image import ImageDataGenerator  # questionavel a utilização
+
+classificador = Sequential()
+
+classificador.add(
+    Conv2D(  # primeira camada de convolução
+        64,  # número de filtro
+        (9, 9),  # dimensão do detectores de caracteristicas
+        # Altura e largura da img e numero de canais (3 significa RGB) - converção das imagens originais para este padrão
+        input_shape=(320, 320, 3),
+        activation='relu'  # função de ativação - tirar os valores negativos, que representam as partes mais escuras das imagens
+    ))
